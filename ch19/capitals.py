a0eaab9c523cd0af8877dc6175fd4a60ab000cb5
Bill Lubanovic <bill@madscheme.com>
2020-03-21 21:39:18 -0500

    Indents

--- ch19/capitals.py
+++ ch19/capitals.py
@@ -1,13 +1,13 @@
 def process_cities(filename):
     with open(filename, 'rt') as file:
-    for line in file:
-        line = line.strip()
-        if 'quit' in line.lower():
-            return
-        country, city = line.split(',')
-        city = city.strip()
-        country = country.strip()
-        print(city.title(), country.title(), sep=',')
+        for line in file:
+            line = line.strip()
+            if 'quit' in line.lower():
+                return
+            country, city = line.split(',')
+            city = city.strip()
+            country = country.strip()
+            print(city.title(), country.title(), sep=',')
 
 if __name__ == '__main__':
     import sys
