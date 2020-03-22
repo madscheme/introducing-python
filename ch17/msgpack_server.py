0c95f4bbfa04c9ad706d45aed0f84b553b3ce97b
Bill Lubanovic <bill@madscheme.com>
2020-03-21 21:38:14 -0500

    Indents

--- ch17/msgpack_server.py
+++ ch17/msgpack_server.py
@@ -2,7 +2,7 @@ from msgpackrpc import Server, Address
 
 class Services():
     def double(self, num):
-    return num * 2
+        return num * 2
 
 server = Server(Services())
 server.listen(Address("localhost", 6789))
--- ch17/zerorpc_server.py
+++ ch17/zerorpc_server.py
@@ -2,7 +2,7 @@ import zerorpc
 
 class RPC():
     def double(self, num):
-    return 2 * num
+         return 2 * num
 
 server = zerorpc.Server(RPC())
 server.bind("tcp://0.0.0.0:4242")
